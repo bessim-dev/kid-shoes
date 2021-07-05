8698271f61a43f8c891baa74d7eba946db9b74ef
becem-dev <you@example.com>
2021-07-05 13:26:10 +0100

    change folder name to client

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
